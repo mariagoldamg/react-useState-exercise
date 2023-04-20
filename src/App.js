cd8c3e81e6044ca55e1104d012891ebb7cd9c460
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-20 09:38:57 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {data} from './data'
 import './App.css';
 
 function App() {
+
+  const [vinil, setVinil] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     {vinil.map((thing =>{
+      const {id, toListenTo} = thing;
+      return (
+        <div key={id}>
+          <p>{id} - {toListenTo}</p>
+        </div>
+      )
+     }))} 
+     <button onClick = {()=> setVinil([])}> Delete All</button>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        id:1,
+        toListenTo: "Lana Del Ray"
+    },
+ {
+        id:2,
+        toListenTo: "Madonna"
+    },
+     {
+        id:3,
+        toListenTo: "Britney Spears"
+    },
+     {
+        id:4,
+        toListenTo: "Beyonce"
+    },
+     {
+        id:5,
+        toListenTo: "Shakira"
+    }
+]
